94ce395c2f252b23a60f2afbe0c2e16429a9ff3b
Marisha-tech <34@3389.ru>
2021-11-18 16:15:05 +0700

    1. Как работает RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {interval} from 'rxjs' // вид импорта из библиотеки rxjs. interval
+// import {} from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'RxJSBasic';
+  constructor() {
+
+    const intervalStream$ = interval(1000) //знаком $ принято обозначать те переменные, в которых содержится реактивность
+
+    intervalStream$.subscribe((value) => {
+      console.log(value)
+    })
+
+  }
 }
